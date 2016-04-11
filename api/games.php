b6598a414d5d232f8e750a472631c7d40cc2d467
MolodetsTovarish <misha.okner@gmail.com>
2016-04-11 16:44:13 -0400

    first commit

---
+++ api/games.php
@@ -0,0 +1,34 @@
+<?php
+// connect
+require_once('../db.php');
+
+
+
+// select the games and store the results
+$sql = "SELECT * FROM games";
+
+// check for a specific game name in the url
+if (!empty($_GET['name'])) {
+    $sql .= " WHERE name = :name";
+}
+
+$cmd = $conn->prepare($sql);
+
+if (!empty($_GET['name'])) {
+    $cmd->bindParam(':name', $_GET['name']);
+}
+// select the games and store the results
+$sql = "SELECT * FROM games";
+
+$cmd->execute();
+$games = $cmd->fetchAll();
+
+// convert the php data array to json
+$json_obj = json_encode($games);
+
+// display the data
+echo $json_obj;
+
+// disconnect
+$conn = null;
+?>
